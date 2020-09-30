5b348a207748b970332415b01e54201710a3041d
Renukatadavarthi <72133417+Renukatadavarthi@users.noreply.github.com>
2020-09-30 15:51:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Router1
+# Pipe
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/router1'),
+      dir: require('path').join(__dirname, './coverage/pipe'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
